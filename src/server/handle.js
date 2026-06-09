de56895a26e1809db1b52d087aab1fbafd453798
Nigel Vining <nigel.vining@agiledata.io>
2026-06-10 09:06:26 +1200

    SECURITY: add recommended security headers to frontend (already applied to backend)

--- src/server/handle.js
+++ src/server/handle.js
@@ -32,5 +32,11 @@ export async function handle({ event, resolve }) {
 
   const response = await resolve(event);
   response.headers.delete('link');
+  response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+  response.headers.set('X-Frame-Options', 'DENY');
+  response.headers.set('X-Content-Type-Options', 'nosniff');
+  response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
+  response.headers.set('Cross-Origin-Opener-Policy', 'same-origin');
+  response.headers.set('Cross-Origin-Resource-Policy', 'same-origin');
   return response;
 }
